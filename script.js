83429e878095f3958d0a909bb1a3182d62116f33
filippo <tosifilippo@outlook.it>
2021-02-16 18:27:23 +0100

    fixed the localstorage issue by setting each element proto of the retrieved array to new Book

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const userPages = document.getElementById("pages");
 const userRead = document.getElementsByName("read/not read");
 
 let myLibrary = [];
+let storedLibrary = [];
 
 function Book (title, author, pages, read) {
     this.title = title;
@@ -14,56 +15,6 @@ function Book (title, author, pages, read) {
         return this.title + " di " + this.author + ". " + this.pages + ' pagine, ' + this.read + '.';
     }; 
 };
-
-function saveLocal() {
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-  }
-  
-  function restoreLocal() {
-    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
-    if (myLibrary === null) myLibrary = [];
-};
-  
-restoreLocal();
-
-// const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
-// const iSentieri = new Book('I sentieri', 'Italo Calvino', '58', 'already read');
-// const leNotti = new Book('Le Notti Bianche', 'Fedor Dostoevskij', '95', 'not read yet');
-// myLibrary.push(theHobbit);
-// myLibrary.push(iSentieri);
-// myLibrary.push(leNotti);
-
-myLibrary.forEach(element => { 
-    let para = document.createElement("p");   
-    para.innerHTML = element.info();                  
-    document.body.appendChild(para);
-    let deleteButton = document.createElement("button");
-    deleteButton.setAttribute('data-index', myLibrary.indexOf(element));
-    deleteButton.innerText = "Delete Book";
-    deleteButton.onclick = function removeBookFromLibrary() {
-        delete myLibrary[deleteButton.dataset.index];
-        saveLocal();
-        document.body.removeChild(para);
-    };    
-    para.appendChild(deleteButton);
-    let toggleReadButton = document.createElement("button");
-    toggleReadButton.setAttribute('data-index', myLibrary.indexOf(element));
-    toggleReadButton.innerText = "Letto/Non letto"
-    toggleReadButton.onclick = function toggleRead() {
-        if (myLibrary[toggleReadButton.dataset.index].read == "già letto") {
-            myLibrary[toggleReadButton.dataset.index].read = "non ancora letto";
-            saveLocal();
-            para.innerText = myLibrary[toggleReadButton.dataset.index].info();
-        } else if (myLibrary[toggleReadButton.dataset.index].read == "non ancora letto") {
-            myLibrary[toggleReadButton.dataset.index].read = "già letto";
-            saveLocal();
-            para.innerText = myLibrary[toggleReadButton.dataset.index].info();
-        };
-        para.appendChild(toggleReadButton);
-        para.appendChild(deleteButton);
-    };
-    para.appendChild(toggleReadButton);
-}); 
 
 function addBookToLibrary() {
     if (userRead[0].checked) {
@@ -103,3 +54,55 @@ function addBookToLibrary() {
     };
     para.appendChild(toggleReadButton);
 };
+
+function saveLocal() {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+};
+  
+function restoreLocal() {
+    myLibrary = (JSON.parse(localStorage.getItem("myLibrary")));
+    if (myLibrary === null) myLibrary = [];
+    myLibrary.forEach(element => {
+        if (element != null) {
+        element.__proto__  = new Book;
+        } else {
+            
+        }
+    })
+};
+  
+restoreLocal();
+
+myLibrary.forEach(element => {
+    if (element != null) {
+    let para = document.createElement("p");   
+    para.innerHTML = element.info();                  
+    document.body.appendChild(para);
+    let deleteButton = document.createElement("button");
+    deleteButton.setAttribute('data-index', myLibrary.indexOf(element));
+    deleteButton.innerText = "Delete Book";
+    deleteButton.onclick = function removeBookFromLibrary() {
+        delete myLibrary[deleteButton.dataset.index];
+        saveLocal();
+        document.body.removeChild(para);
+    };    
+    para.appendChild(deleteButton);
+    let toggleReadButton = document.createElement("button");
+    toggleReadButton.setAttribute('data-index', myLibrary.indexOf(element));
+    toggleReadButton.innerText = "Letto/Non letto"
+    toggleReadButton.onclick = function toggleRead() {
+        if (myLibrary[toggleReadButton.dataset.index].read == "già letto") {
+            myLibrary[toggleReadButton.dataset.index].read = "non ancora letto";
+            saveLocal();
+            para.innerText = myLibrary[toggleReadButton.dataset.index].info();
+        } else if (myLibrary[toggleReadButton.dataset.index].read == "non ancora letto") {
+            myLibrary[toggleReadButton.dataset.index].read = "già letto";
+            saveLocal();
+            para.innerText = myLibrary[toggleReadButton.dataset.index].info();
+        };
+        para.appendChild(toggleReadButton);
+        para.appendChild(deleteButton);
+    };
+    para.appendChild(toggleReadButton);
+    };
+}); 
